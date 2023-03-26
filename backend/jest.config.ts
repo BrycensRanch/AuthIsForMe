c74498af5b610e41ca2fa4bc9608dbfe03bae188
BrycensRanch <brycengranville@outlook.com>
2023-03-26 09:29:27 -0400

    test: break tests more

---
+++ backend/jest.config.ts
@@ -0,0 +1,44 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+export const coverageThreshold = {
+	global: {
+		lines: 80,
+		statements: 80,
+		branches: 70,
+		functions: 80,
+	},
+};
+
+const jestConfig: JestConfigWithTsJest = {
+	preset: "ts-jest/presets/js-with-ts-esm",
+	testEnvironment: "node",
+	resolver: "jest-ts-webcompat-resolver",
+	clearMocks: true,
+	collectCoverage: true,
+  collectCoverageFrom: [
+    './routes/**/*.{js,ts, mts, mjs, cjs, cts}',
+    './plugins/**/*.{js,ts, mts, mjs, cjs, cts}',
+    './app.{js,ts, mts, mjs, cjs, cts}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+	testMatch: ["**/*.test.ts"],
+	moduleNameMapper: {
+      // Handle module aliases (this will be automatically configured for you soon)
+		"^@/(.*)$": "<rootDir>/src/$1",
+  '^@/public/(.*)$': '<rootDir>/public/$1',
+	},
+	verbose: true,
+	transform: {
+		// ".(t|j)s$": "babel-jest",
+		// '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+		// '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+		"^.+\\.m?[tj]sx?$": [
+			"ts-jest",
+			{
+				useESM: true,
+			},
+		],
+	},
+};
+export default jestConfig;
--- backend/test/integration/plugins/errorHandler.test.ts
+++ backend/test/integration/plugins/errorHandler.test.ts
@@ -3,7 +3,7 @@ import {expect, jest, test} from '@jest/globals';
 
 
 // eslint-disable-next-line import/extensions
-import ErrorHandler from '../../plugins/errorHandler';
+import ErrorHandler from '../../../src/plugins/errorHandler';
 
 test('error handler works standalone', async () => {
   const fastify = Fastify();
