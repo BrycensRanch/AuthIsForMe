e2fd92b263b426464fc10f65f28cec1c930bb944
BrycensRanch <brycengranville@outlook.com>
2023-04-03 15:15:52 -0400

    chore(repo): do not run on js files

--- frontend/lint-staged.config.js
+++ frontend/lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"*.{js,jsx,ts,tsx}": () => "pnpm lint-staged",
+	"*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
 	"**/*.ts?(x)": () => "pnpm check-types",
-	"*.{json,md,yaml,yml}": "prettier --write",
+	"*.{json,md,yaml,yml,css}": "prettier --write",
 };
