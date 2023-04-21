cbaed0ac9755026cdefead044f2bf1df1231daa8
BrycensRanch <brycengranville@outlook.com>
2023-04-21 01:54:30 -0400

    refactor(frontend): still broken

--- backend/src/modules/root.controller.test.ts
+++ backend/src/modules/root.controller.test.ts
@@ -11,13 +11,6 @@ describe('RootController', () => {
 	});
 
 	it('should be defined', () => {
-    expect(rootController).toBeDefined();
-  });
-  it('should work', async function () {
-    const result = rootController.root();
-    expect(result).toBeDefined();
-  });
-  it('should work as a module', async function () {
-
-  })
+		expect(rootController).toBeDefined();
+	});
 });
